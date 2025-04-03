68ebfba78664ca058bed11926176d42225311492
Himangshu <frenzy9599@gmail.com>
2025-04-04 00:34:09 +0530

    added the text scroll animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,10 @@
 window.onload= function(){
+let time =0;
 const body = document.getElementsByTagName('body');
 (function(){
     body[0].classList.add('overflow-hidden')
 })();
 setTimeout(() => {
     body[0].classList.remove('overflow-hidden')
-}, 6000);
+}, time);
 }
